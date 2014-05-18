6a6afbc763d6314ea073dbea449e41f69bab75dd
Graham <graham_howden1@yahoo.co.uk>
2014-05-18 16:15:46 +0100

    add docs

--- src/main/java/com/publicuhc/custombows/EntityRemoveJob.java
+++ src/main/java/com/publicuhc/custombows/EntityRemoveJob.java
@@ -4,6 +4,9 @@ import org.bukkit.entity.Entity;
 
 import java.lang.ref.WeakReference;
 
+/**
+ * Creates a runnable that will remove the entity when it runs, will do nothing if the entity no longer exists
+ */
 public class EntityRemoveJob implements Runnable {
 
     private final WeakReference<Entity> m_entity;
@@ -11,7 +14,6 @@ public class EntityRemoveJob implements Runnable {
     public EntityRemoveJob(Entity entity) {
         m_entity = new WeakReference<Entity>(entity);
     }
-
 
     @Override
     public void run() {
