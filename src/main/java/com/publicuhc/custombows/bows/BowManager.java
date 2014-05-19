0b9da448055b93302961b06975929b5eab8a90bc
HowdenG <graham_howden1@yahoo.co.uk>
2014-05-19 09:44:15 +0100

    start of events

--- src/main/java/com/publicuhc/custombows/bows/BowManager.java
+++ src/main/java/com/publicuhc/custombows/bows/BowManager.java
@@ -1,5 +1,8 @@
 package com.publicuhc.custombows.bows;
 
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.inventory.ItemStack;
+
 public interface BowManager {
 
     /**
@@ -21,4 +24,17 @@ public interface BowManager {
      * @return true if found, false if not
      */
     boolean doesBowTypeExist(String id);
+
+    /**
+     * Called when a projectile is launched, will detect if special bow and will do the rest
+     * @param ple the event
+     */
+    void onProjectileLaunchEvent(ProjectileLaunchEvent ple);
+
+    /**
+     * Get the bow type applicable to the item
+     * @param item the item to check
+     * @return the bow type if exists, null otherwise
+     */
+    BowType getBowTypeForItemStack(ItemStack item);
 }
--- src/main/java/com/publicuhc/custombows/bows/DefaultBowManager.java
+++ src/main/java/com/publicuhc/custombows/bows/DefaultBowManager.java
@@ -1,9 +1,18 @@
 package com.publicuhc.custombows.bows;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.projectiles.ProjectileSource;
+
 import java.util.HashSet;
 import java.util.Set;
 
-public class DefaultBowManager implements BowManager {
+public class DefaultBowManager implements BowManager, Listener {
 
     private Set<BowType> m_types = new HashSet<BowType>();
 
@@ -27,5 +36,34 @@ public class DefaultBowManager implements BowManager {
         return false;
     }
 
+    @Override
+    @EventHandler
+    public void onProjectileLaunchEvent(ProjectileLaunchEvent ple) {
+        if(!(ple.getEntity() instanceof Arrow)) {
+            return;
+        }
+        Arrow arrow = (Arrow) ple.getEntity();
 
+        ProjectileSource source = arrow.getShooter();
+        if(!(source instanceof LivingEntity)) {
+            return;
+        }
+        LivingEntity shooter = (LivingEntity) source;
+
+        ItemStack held = shooter.getEquipment().getItemInHand();
+
+        //Stop a 'quick switch' after shooting from doing anything
+        //otherwise you can shoot a normal arrow from any bow type
+        if(held.getType() != Material.BOW) {
+            ple.setCancelled(true);
+            return;
+        }
+
+        //TODO find the bow type and apply it's effects
+    }
+
+    @Override
+    public BowType getBowTypeForItemStack(ItemStack item) {
+        return null;
+    }
 }
