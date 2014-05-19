ad8c0c465e149bfc7451c1c2e7e02d0a4bcae101
HowdenG <graham_howden1@yahoo.co.uk>
2014-05-19 10:12:45 +0100

    set up a shotgun bow with fire arrows with 10 variance

--- src/main/java/com/publicuhc/custombows/CustomBows.java
+++ src/main/java/com/publicuhc/custombows/CustomBows.java
@@ -1,11 +1,24 @@
 package com.publicuhc.custombows;
 
+import com.publicuhc.custombows.arrows.FireArrowBehaviour;
+import com.publicuhc.custombows.arrows.RandomOffsetArrowBehaviour;
+import com.publicuhc.custombows.bows.BowManager;
+import com.publicuhc.custombows.bows.DefaultBowManager;
+import com.publicuhc.custombows.bows.ShotgunBow;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CustomBows extends JavaPlugin {
 
     @Override
 	public void onEnable() {
+        BowManager manager = new DefaultBowManager();
+        getServer().getPluginManager().registerEvents(manager, this);
+
+        ShotgunBow shotgun = new ShotgunBow(this);
+        shotgun.addBehaviour(new RandomOffsetArrowBehaviour(this, 10, 10));
+        shotgun.addBehaviour(new FireArrowBehaviour());
+
+        manager.addBowType(shotgun);
 	}
 
 }
