10c6cb9c0a0039f5e4d77cb92ea048fff2b93cb0
Graham <graham_howden1@yahoo.co.uk>
2014-05-20 20:49:49 +0100

    only allow full drawback on the shotgun

--- src/main/java/com/publicuhc/custombows/bows/ShotgunBow.java
+++ src/main/java/com/publicuhc/custombows/bows/ShotgunBow.java
@@ -2,9 +2,11 @@ package com.publicuhc.custombows.bows;
 
 import com.publicuhc.custombows.arrows.DelayedArrowSpawnRunnable;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Sound;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.util.Vector;
 
@@ -21,7 +23,20 @@ public class ShotgunBow extends BowType {
 
     @Override
     public void triggerShot(Arrow original, LivingEntity shooter) {
-        //get a copy of the original arrow
+        if(shooter instanceof Player) {
+            Player player = (Player) shooter;
+            //only allow full pull back
+            if(original.getVelocity().length() < 2.8){
+                player.sendMessage(ChatColor.RED + "Pull the bow all the way back to use the shotgun!");
+                player.playSound(player.getLocation(), Sound.ITEM_BREAK,1,0);
+                //dont fire anything
+                original.remove();
+                return;
+            }
+        }
+
+
+        //get a copy of the original arrow velocity
         Vector regular = original.getVelocity().clone();
 
         original.getWorld().playSound(original.getLocation(), Sound.EXPLODE, 4, 0);
