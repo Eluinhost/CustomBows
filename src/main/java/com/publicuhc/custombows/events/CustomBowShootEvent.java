b9864fa26b68790f9a13541a693836b775cd2b51
HowdenG <graham_howden1@yahoo.co.uk>
2014-05-22 15:04:31 +0100

    make the event use the arrow not the player

--- src/main/java/com/publicuhc/custombows/events/CustomBowShootEvent.java
+++ src/main/java/com/publicuhc/custombows/events/CustomBowShootEvent.java
@@ -1,7 +1,7 @@
 package com.publicuhc.custombows.events;
 
 import com.publicuhc.custombows.bows.BowType;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.Arrow;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
@@ -9,12 +9,12 @@ import org.bukkit.event.HandlerList;
 public class CustomBowShootEvent extends Event implements Cancellable {
 
     private static final HandlerList HANDLER_LIST = new HandlerList();
-    private Player m_player;
+    private Arrow m_arrow;
     private boolean m_cancelled = false;
     private final BowType m_type;
 
-    public CustomBowShootEvent(Player player, BowType type){
-        m_player = player;
+    public CustomBowShootEvent(Arrow arrow, BowType type){
+        m_arrow = arrow;
         m_type = type;
     }
 
@@ -40,10 +40,16 @@ public class CustomBowShootEvent extends Event implements Cancellable {
         m_cancelled = cancelled;
     }
 
-    public Player getPlayer() {
-        return m_player;
+    /**
+     * @return the arrow that started the event
+     */
+    public Arrow getArrow() {
+        return m_arrow;
     }
 
+    /**
+     * @return the bow type that was matched for this event
+     */
     public BowType getType() {
         return m_type;
     }
